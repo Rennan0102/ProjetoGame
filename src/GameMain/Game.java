47ab91050ee7e2693a36fb5c479c4204f2cb319f
Rennan <rennan.fernandess@gmail.com>
2023-06-06 09:57:44 -0300

    Primeiro commit

--- src/GameMain/Game.java
+++ src/GameMain/Game.java
@@ -1,12 +1,19 @@
 package GameMain;
 
-public class Game {
+public class Game implements Runnable{
 	private GameWindow gameWindow;
 	private GamePanel gamePanel;
+	private Thread gameThread;
 	
 	public Game() {
 		gamePanel = new GamePanel();
 		gameWindow = new GameWindow(gamePanel);
 		gamePanel.requestFocus();  // Aqui damos um "foco", assim, quando fornecemos alguma entrada ela é capturada
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 }
